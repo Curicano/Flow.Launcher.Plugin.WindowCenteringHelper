ff95c327bbf29c5520192289fda2f8275e73e594
Curicano <95356567+Curicano@users.noreply.github.com>
2024-02-09 19:31:44 +1000

    /

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import sys, os
 
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
--- plugin/wch.py
+++ plugin/wch.py
@@ -3,6 +3,14 @@ from pygetwindow import getAllWindows, PyGetWindowException
 from flowlauncher import FlowLauncher
 
 
+# import logging
+# logging.basicConfig(filename="log.log",
+#                     filemode="a",
+#                     level=logging.INFO,
+#                     encoding="utf-8",
+#                     format="%(asctime)s - %(levelname)s - %(message)s")
+
+
 class WindowCenteringHelper(FlowLauncher):
     ICON_PATH = "Images/app.png"
     query_results = []
@@ -10,20 +18,6 @@ class WindowCenteringHelper(FlowLauncher):
     def query(self, query) -> list:
         self.getAllWindows(query)
         return self.query_results
-
-    def context_menu(self, data):
-        return [
-            {
-                "title": "Place in the center and resize",
-                "subTitle": r"80% of the total screen size",
-                "icoPath": self.ICON_PATH,
-                "jsonRPCAction": {
-                    "method": "centeringWindow",
-                    "parameters": data
-                },
-                "score": 0
-            }
-        ]
 
     def addMessage(self, title: str, method: str = "", parameters: str = "", subtitle: str = "") -> None:
         self.query_results.append(
@@ -40,7 +34,20 @@ class WindowCenteringHelper(FlowLauncher):
             }
         )
 
-    def getAllWindows(self, title: str = "") -> None:
+    def getAllWindows(self, query: str) -> None:
+        if len(query.split()) > 1:
+            if query.rsplit(None, 1)[-1].isdigit():
+                title, size = query.rsplit(None, 1)
+                if int(size) > 100:
+                    size = 100
+                elif int(size) < 20:
+                    size = 20
+            else:
+                title = query
+                size = None
+        else:
+            title = query
+            size = None
         all_windows = getAllWindows()
         for window in all_windows:
             if window.title:
@@ -48,26 +55,27 @@ class WindowCenteringHelper(FlowLauncher):
                     if not window.isMinimized:
                         if window.visible:
                             if title.lower() in window.title.lower():
-                                self.addMessage(title=f"{window.title} ({window._hWnd})",
+                                self.addMessage(title=f"{window.title} {size if size else ''}",
                                                 method="centeringWindow",
-                                                parameters=window._hWnd
+                                                parameters=[
+                                                    window._hWnd, size]
                                                 )
 
-    def resizeWindow(self, window) -> None:
+    def resizeWindow(self, window, percent: int) -> None:
         screen_width, screen_height = size()
-        new_width = int(screen_width/100*80)
-        new_height = int(screen_height/100*80)
+        new_width = int(screen_width/100*percent)
+        new_height = int(screen_height/100*percent)
         try:
             window.resizeTo(new_width, new_height)
         except PyGetWindowException:
             pass
 
-    def centeringWindow(self, hwnd: str, resize: bool = False) -> None:
+    def centeringWindow(self, args: list) -> None:
         all_windows = getAllWindows()
         for window in all_windows:
-            if window._hWnd == hwnd:
-                if resize:
-                    self.resizeWindow(window)
+            if window._hWnd == args[0]:
+                if args[1]:
+                    self.resizeWindow(window, int(args[1]))
                 screen_width, screen_height = size()
                 new_win_x, new_win_y = (
                     (screen_width - window.width) / 2), ((screen_height - window.height) / 2)
